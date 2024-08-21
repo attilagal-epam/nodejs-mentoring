ac5e49ba9b1afbddcef88b1d712db96759f07abb
Attila Gal <attila_gal@epam.com>
2024-08-21 17:10:20 +0200

    fix number generation

--- module1/index.js
+++ module1/index.js
@@ -1,8 +1,7 @@
 // Description: This module exports a function that returns a random number between 0 and 1000.
 /*
-Parameters: n Number    
-Returns: random number between 0 and 1000
+Returns: random number between 1 and 1000
 */
 export default function getRandomNumber() {
-    return Math.floor(Math.random() * 1000);
+    return Math.floor(Math.random() * 1000) + 1;
 }
