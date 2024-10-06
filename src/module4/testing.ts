4a93b1c33d5a57bfdb234b290478aa349872eadd
Attila Gal <attila_gal@epam.com>
2024-10-06 20:55:17 +0200

    add solution runner to module

--- src/module4/testing.ts
+++ src/module4/testing.ts
@@ -1,4 +1,21 @@
+import childProcess from "child_process";
+
 export const run = () => {
     console.log("running module 4");
-    return "testing";
+    let fullData = "";
+    let dataChumks = 0;
+    const process = childProcess.execFile('npm run test:all', { shell: true });
+    process.stdout?.on('data', (data) => {
+        fullData += data;
+        dataChumks++;
+        console.log(data);
+    });
+    process.stderr?.on('data', (data) => {
+        console.error(data);
+    });
+    process.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+        console.log(`data chunks: ${dataChumks}`);
+        console.log(fullData);
+    });
 }
